39a332ba7a8b7aefb34b55a5223b42fef5d56264
Rishav Pandey <pandeyrishav044@gmail.com>
2026-08-25 10:25:31 +0530

    cii

--- BOOK-CDN-APP/index.js
+++ BOOK-CDN-APP/index.js
@@ -1,11 +1,33 @@
-function Book(){
-    const image=React.createElement("img",{src:"",width:"100px",height:"100px"},null);
-    const title=React.createElement("h2",{color:"red"},"Title: ReactJS");
-    const price=React.createElement("h2",{color:"Green"},"Price:465/-");
-    const btn=React.createElement("button",{color:"blue"},"Add to Cart");
-    const div=React.createElement("div",{className:"book"},[image,title,price,btn]);
+function Book(props){
+    const image=React.createElement("img",
+        {src:props.image,width: "100px",height:"100px"},null);
+    const title=React.createElement("h2",
+           {style:{color: "red"}},"Title:"+props.title)
+    const price=React.createElement("h2",
+           {style:{color: "Green"}},"Price:"+props.price+"/-")
+    const btn=React.createElement("button",
+           {style:{color: "blue"}},"AddToCart")
+    const div=React.createElement("div",
+        {className:"book"},[image,title,price,btn])
+    return div;0                      
+}
+const bookdata=[
+    {image: "",title:"ReactJS",price: 465},
+    {image: "",title:"NodeJS",price: 578},
+    {image: "",title:"ExpreeJS",price: 963},
+     {image: "",title:"ReactJS",price: 465},
+    {image: "",title:"NodeJS",price: 578},
+    {image: "",title:"ExpreeJS",price: 963},
+]
+function App(){
+    const bookstore=bookdata.map((b)=>{
+      return Book(b)
+    })
+    const div=React.createElement("div",
+        {className:"bookstore"},[...bookstore]
+    )
     return div;
 }
 const parent=document.getElementById("root");
 const root=ReactDOM.createRoot(parent);
-root.render(Book());
+root.render(App())
